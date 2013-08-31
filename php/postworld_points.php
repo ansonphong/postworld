18717bb2a4b80f6c57e635686f6452f373037c0d
Asun Phong <asun@phong.com>
2013-08-30 20:23:13 -0700

    points

---
+++ php/postworld_points.php
@@ -0,0 +1,90 @@
+<?php
+
+get_points ( $post_id ){
+	/*
+	• Get the total number of points of the given post from the points column in 'wp_postworld_meta' table
+	return : integer
+	*/
+}
+
+set_points ( $post_id, $user_id, $add_points ){
+	/*
+	• $add_points is an integer
+	• Write row in wp_postworld_points
+	• Passing 0 deletes row
+	• Check that user role has permission to write that many points (wp_options) <<<<
+	• Check is the user has already voted points on that post
+	• Also update cached points in wp_postworld_meta directly
+	• Add Unix timestamp to time column in wp_postworld_points
+	return : Object
+	     points_added : {{integer}} (points which were successfully added)
+	     points_total : {{integer}} (from wp_postworld_meta)
+	*/
+}
+
+
+
+calculate_points ( $post_id ){
+	/*
+	• Adds up the points from the specified post, stored in wp_postworld_points
+	• Stores the result in the points column in wp_postworld_meta
+	return : integer (number of points)
+	*/
+}
+
+
+has_voted ( $post_id, $user_id ){
+	/*
+	• Check wp_postworld_points to see if the user has voted on the post
+	• Return the number of points
+	return : integer
+	*/
+}
+
+
+get_user_points ( $user_id ){
+	/*
+	• Get array of all posts by given user
+	• Get points of each post from wp_postworld_meta
+	• Add all the points up
+	return : integer (number of points)
+	*/
+}
+
+
+
+get_user_votes ( $user_id ){
+	/*
+	• Returns the 'recent/active' points activity of the user
+	• Get all posts which user has recently voted on from wp_postworld_points ( total_posts )
+	• Add up all points cast (total_points)
+	• Generate average (total_points/total_posts) 
+	return : Object
+	     total_posts: {{integer}} (number of posts voted on)
+	     total_points: {{integer}} (number of points cast by up/down votes)
+	     average_points: {{decimal}} (average number of points per post)
+	*/
+}
+
+
+get_user_votes ( $user_id ){
+	/*
+	• Get all posts which user has voted on from wp_postworld_points
+	return : Object
+	     #for_each
+	     post_id : {{integer}}
+	     votes : {{integer}}
+	     time : {{timestamp}}
+	*/
+}
+
+
+cache_points () {
+	/*
+	• Cycles through each post in each post_type with points enabled
+	• Calculates each post's current rank with calculate_points()
+	• Stores points it in wp_postworld_meta 'points' column
+	*/
+}
+
+?>
--- postworld.php
+++ postworld.php
@@ -1,8 +1,8 @@
 <?php
 /******************************************
 Plugin Name: Post World
-Plugin URI: htp://phong.com/plugins/postworld
-Description: Settings for Post World 2 Wordpress Configuration
+Plugin URI: htp://phong.com/
+Description: Postworld extends Wordpress to display posts in creative ways
 Version: 2.0
 Author: phong
 Author URI: http://phong.com
@@ -10,80 +10,13 @@ License: GPL2
 ******************************************/
 
 ////////// INSTALL POSTWORLD ///////////
-//include 'postworld_install.php';
-
-
-global $postworld_db_version;
-$postworld_db_version = "1.0";
-
-function postworld_install() {
-  global $wpdb;
-  global $postworld_db_version;
-
-  $pw_prefix = "postworld_";
-
-  $meta_table_name = $wpdb->prefix . $pw_prefix .  "meta";
-  $points_table_name = $wpdb->prefix . $pw_prefix . "points";
-  $user_meta_table_name = $wpdb->prefix . $pw_prefix . "user_meta";
-  $user_shares_table_name = $wpdb->prefix . $pw_prefix . "user_shares";
-    
-  $sql_postworld_meta = "CREATE TABLE $meta_table_name (
-      id mediumint(9) NOT NULL,
-      post_class char(16) NOT NULL,
-      post_format char(16) NOT NULL,
-      link_url varchar(512) DEFAULT '' NOT NULL,
-      points mediumint(8) DEFAULT '0' NOT NULL,
-      rank_score mediumint(4) DEFAULT '0' NOT NULL,
-      UNIQUE KEY id (id)
-    );";
-
-  $sql_postworld_points = "CREATE TABLE $points_table_name (
-      id mediumint(9) NOT NULL,
-      user_id mediumint(8) NOT NULL,
-      points mediumint(8) DEFAULT '0' NOT NULL,
-      time TIMESTAMP NOT NULL
-    );";
-
-  $sql_postworld_user_meta = "CREATE TABLE $user_meta_table_name (
-      user_id mediumint(9) NOT NULL,
-      user_role char(16) NOT NULL,
-      viewed MEDIUMTEXT NOT NULL, 
-      favorites MEDIUMTEXT NOT NULL, 
-      location_city char(24) NOT NULL,
-      location_country char(24) NOT NULL,
-      view_karma mediumint(8) DEFAULT '0' NOT NULL,
-      share_karma mediumint(8) DEFAULT '0' NOT NULL,
-      UNIQUE (user_id)
-    );";
-
-  $sql_postworld_user_shares = "CREATE TABLE $user_shares_table_name (
-      user_id mediumint(9) NOT NULL,
-      post_id mediumint(9) NOT NULL,
-      recent_ips varchar(8000) DEFAULT '' NOT NULL,
-      total_views mediumint(9) NOT NULL
-    );";
-
-  require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-  dbDelta( $sql_postworld_meta );
-  dbDelta( $sql_postworld_points );
-  dbDelta( $sql_postworld_user_meta );
-  dbDelta( $sql_postworld_user_shares );
-  
-  add_option( "postworld_db_version", $postworld_db_version );
-}
-
-/*
-function postworld_install_data() {
-  global $wpdb;
-  $welcome_name = "Mr. WordPress";
-  $welcome_text = "Congratulations, you just completed the installation!";
-
-  $rows_affected = $wpdb->insert( $table_name, array( 'time' => current_time('mysql'), 'name' => $welcome_name, 'text' => $welcome_text ) );
-}
-*/
-
+include 'php/postworld_install.php';
 register_activation_hook( __FILE__, 'postworld_install' );
-//register_activation_hook( __FILE__, 'jal_install_data' );
+
+
+////////// POINTS FUNCTIONS ///////////
+include 'php/postworld_points.php';
+
+
 
 ?>
