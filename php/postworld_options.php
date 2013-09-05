2bee30f48bba7ebba04c0133e216ff357c5d9452
Asun Phong <asun@phong.com>
2013-09-05 12:46:40 -0700

    options structure

--- php/postworld_options.php
+++ php/postworld_options.php
@@ -2,24 +2,25 @@
 
 $pw_default_options = array(
 
+	'roles' 			=> array(
+		'Administrator' 	=> array(
+			'vote_points'	=> 10,
+			),
+		'Editor' 			=> array(
+			'vote_points'	=> 5,
+			),
+		'Author' 			=> array(
+			'vote_points'	=> 2,
+			),
+		'Contributor' 		=> array(
+			'vote_points'	=> 1,
+			),
+		),
+
 	'points' => array(
 		'post_types'		=> array('post'),
 		'cache_interval'	=> 'fifteen_minutes',
 		'cron_logs'			=> 0,
-		'roles' 			=> array(
-			'Administrator' 	=> array(
-				'vote_points'	=> 10,
-				),
-			'Editor' 			=> array(
-				'vote_points'	=> 5,
-				),
-			'Author' 			=> array(
-				'vote_points'	=> 2,
-				),
-			'Contributor' 		=> array(
-				'vote_points'	=> 1,
-				),
-			)
 		),
 
 	'rank' => array(
