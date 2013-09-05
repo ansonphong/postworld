0160c1eb109caef786660896e8cc8dd144b6bde3
Asun Phong <asun@phong.com>
2013-09-05 12:44:43 -0700

    added default rank equation

--- php/postworld_options.php
+++ php/postworld_options.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $pw_default_options = array(
@@ -27,6 +26,20 @@ $pw_default_options = array(
 		'post_types'		=> array('post'),
 		'cache_interval'	=> 'fifteen_minutes',
 		'cron_logs'			=> 0,
+		'equations'			=> array(
+			'default'		=> array(
+				'time_compression'	=>	0.5,
+				'time_weight'		=>	1,
+				'comments_weight'	=>	1,
+				'points_weight'		=>	1,
+				'fresh_period'		=>	1*$one_week,
+				'fresh_multiplier'	=>	2,
+				'archive_period'	=>	6*$one_month,
+				'archive_multiplier'=>	0.2,
+				'free_rank_score'	=>	100,
+				'free_rank_period'	=>	3*$one_day,
+				),
+			),
 		),
 
 	'feeds' => array(
