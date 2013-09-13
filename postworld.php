164294c7f692c032e22260d606616ba03f4c92f5
Asun Phong <asun@phong.com>
2013-09-13 16:48:55 -0700

    removed wp includes from postworld.php

--- postworld.php
+++ postworld.php
@@ -70,9 +70,9 @@ include 'php/postworld_options.php';
 ////////// INSTALL POSTWORLD ///////////
 include 'php/postworld_install.php';
 register_activation_hook( __FILE__, 'postworld_install' );
-//register_activation_hook( __FILE__, 'postworld_install_data' );
-//register_activation_hook( __FILE__, 'postworld_install_Foreign_keys' );
-//register_activation_hook( __FILE__, 'postworld_install_Triggers' );
+register_activation_hook( __FILE__, 'postworld_install_data' );
+register_activation_hook( __FILE__, 'postworld_install_Foreign_keys' );
+register_activation_hook( __FILE__, 'postworld_install_Triggers' );
 
 ////////// META FUNCTIONS ///////////
 include 'php/postworld_meta.php';
@@ -97,15 +97,12 @@ include 'php/postworld_images.php';
 include 'php/postworld_posts.php';
 
 
-
+/*
 //TO get user id from wordpress
 require_once('/wp-config.php');
 require_once('/wp-includes/wp-db.php');
 require_once('/wp-includes/pluggable.php');
-
-
-
-
+*/
 
 
 
