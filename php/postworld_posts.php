a1738733df34489567c2df2f8a995d83d6aebeff
hmikhail <eng.haidy.mikhail@gmail.com>
2013-10-08 18:47:08 +0200

    pw_update_post

--- php/postworld_posts.php
+++ php/postworld_posts.php
@@ -604,4 +604,63 @@ function pw_insert_post ( $postarr, $wp_error = TRUE ){
 	
 }
 
+function pw_update_post ( $postarr ,$wp_error = TRUE){
+	/*
+	Extends wp_update_post() : http://codex.wordpress.org/Function_Reference/wp_update_post
+	Include additional Postworld fields as inputs (see pw_insert_post() ) 
+	 */
+	 
+	
+	if ( is_object($postarr) ) {
+		// non-escaped post was passed
+		$postarr = get_object_vars($postarr);
+		$postarr = wp_slash($postarr);
+	}
+
+	// First, get all of the original fields
+	$post = get_post($postarr['ID'], ARRAY_A);
+	
+	//print_r($post);
+
+	if ( is_null( $post ) ) {
+		if ( $wp_error )
+			return new WP_Error( 'invalid_post', __( 'Invalid post ID.' ) );
+		return 0;
+	}
+
+	// Escape data pulled from DB.
+	//$post = wp_slash($post);
+
+	// Passed post category list overwrites existing category list if not empty.
+	if ( isset($postarr['post_category']) && is_array($postarr['post_category'])
+			 && 0 != count($postarr['post_category']) )
+		$post_cats = $postarr['post_category'];
+	else
+		$post_cats = $post['post_category'];
+
+	// Drafts shouldn't be assigned a date unless explicitly done so by the user
+	if ( isset( $post['post_status'] ) && in_array($post['post_status'], array('draft', 'pending', 'auto-draft')) && empty($postarr['edit_date']) &&
+			 ('0000-00-00 00:00:00' == $post['post_date_gmt']) )
+		$clear_date = true;
+	else
+		$clear_date = false;
+
+	// Merge old and new fields with new fields overwriting old ones.
+	$postarr = array_merge($post, $postarr);
+	$postarr['post_category'] = $post_cats;
+	if ( $clear_date ) {
+		$postarr['post_date'] = current_time('mysql');
+		$postarr['post_date_gmt'] = '';
+	}
+
+	if ($postarr['post_type'] == 'attachment')
+		return wp_insert_attachment($postarr);
+
+	
+	//print_r($postarr);
+	return pw_insert_post( $postarr, $wp_error );
+
+	 
+}
+
 ?>
