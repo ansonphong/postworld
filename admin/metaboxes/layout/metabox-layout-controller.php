f850e5574e171826f1fb93700d0068f58915dc66
phong <media@phong.com>
2014-12-11 16:45:49 -0500

    dev layout metabox

--- admin/metaboxes/layout/metabox-layout-controller.php
+++ admin/metaboxes/layout/metabox-layout-controller.php
@@ -25,7 +25,8 @@ global $post;
 		<input type="hidden" name="pw_layout_post" ng-value="pw_layout_post | json" style="width:100%;">
 		
 		<!-- DEV : Test Output 
-		<hr><pre>POST : {{ pw_layout_post | json }}</pre>--> 
+		<hr><pre>POST : {{ pw_layout_post.post_meta.pw_meta.layout | json }}</pre>
+		-->
 		
 	</div>	
 </div>
@@ -51,6 +52,11 @@ global $post;
 			if( !$_.objExists( $scope.pw_layout_post, 'post_meta.<?php echo pw_postmeta_key; ?>.layout' ) )
 				$scope.pw_layout_post = $_.setObj( $scope.pw_layout_post, 'post_meta.<?php echo pw_postmeta_key; ?>.layout', {} );
 
+			// If the Layout object is empty
+			if( _.isEmpty( $_.get( $scope.pw_layout_post, 'post_meta.pw_meta.layout' ) ) )
+				// Make it an object
+				$scope.pw_layout_post = $_.set( $scope.pw_layout_post, 'post_meta.pw_meta.layout', { template : 'default' } );
+
 			// TODO : Add a PW global for pw_postmeta_key and pw_usermeta_key, use that global here
 
 			// ADD : If 'default' template selected, delete the layout object
@@ -66,6 +72,8 @@ global $post;
 	}]);
 	
 </script>
+
+
 
 <?php
 	// Action hook to print the Javascript(s)
