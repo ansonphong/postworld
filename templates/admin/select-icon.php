baf599f3a7cac4c310148cc4ef22bbfe324ea5b1
phong <media@phong.com>
2015-01-23 18:50:00 -0500

    dev select icon

--- templates/admin/select-icon.php
+++ templates/admin/select-icon.php
@@ -1,17 +1,27 @@
 <?php
 	global $pw;
 	$pwInject = $pw['inject'];
+
+	// Get icons if they're defined
+	$icons = _get( $vars, 'icons' );
 ?>
+<script>
+	postworld.controller( 'iconDataCtrl', [ '$scope', function($scope){
+		$scope.customIconOptions = <?php echo json_encode( $icons ) ?>;
+	}]);
+</script>
+
 <!-- DROPDOWN -->
 <span
 	dropdown
 	class="dropdown"
-	pw-ui>
+	pw-ui
+	ng-controller="iconDataCtrl">
 	<!-- SELECTED ITEM -->
 	<span
 		dropdown-toggle
 		class="area-select area-select-icon">
-		<i ng-show="uiBool(<?php echo $vars['ng_model']; ?>)" class="{{ <?php echo $vars['ng_model']; ?> }}"></i>
+		<i ng-show="uiBool(<?php echo $vars['ng_model']; ?>)" class="{{ <?php echo $vars['ng_model']; ?> }} <?php if( $vars['icon_spin'] == true ) echo 'icon-spin' ?>"></i>
 		<span class="select-icon-none" ng-hide="uiBool(<?php echo $vars['ng_model']; ?>)">None</span>
 	</span>
 	<!-- MENU -->
@@ -24,8 +34,19 @@
 			<span>None</span>
 		</li>
 
+		<?php ///// CUSTOM ICONS ARRAY /////
+			if( is_array( $icons ) ){ ?>
+				<li
+					class="select-icon"
+					ng-repeat="icon in ::customIconOptions"
+					ng-click="<?php echo $vars['ng_model']; ?> = icon">
+					<i
+						class="{{ icon }}"></i>
+				</li>
+		<?php } ?>
+
 		<?php ///// ICOMOON /////
-			if( in_array( 'icomoon', $pwInject ) ){ ?>
+			if( in_array( 'icomoon', $pwInject ) && !$icons ){ ?>
 				<li
 					class="select-icon"
 					ng-repeat="icon in ::options.icon.icomoon"
@@ -36,7 +57,7 @@
 		<?php } ?>
 
 		<?php ///// GLYPHICONS /////
-			if( in_array( 'glyphicons-halflings', $pwInject ) ){ ?>
+			if( in_array( 'glyphicons-halflings', $pwInject ) && !$icons ){ ?>
 				<li
 					class="select-icon"
 					ng-repeat="icon in ::options.icon.glyphicons"
