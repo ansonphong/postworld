855fed89a512ea4426bd770b3190213dd6707f09
phong <media@phong.com>
2015-01-26 20:16:14 -0500

    pw-avatar

---
+++ templates/admin/user-meta-pw-avatar.php
@@ -0,0 +1,48 @@
+<?php
+	wp_enqueue_media();
+?>
+<script type="text/javascript">
+	postworldAdmin.controller( 'pwUsermetaAvatarCtrl',
+		[ '$scope', '_', 'iOptionsData',
+		function( $scope, $_, $iOptionsData ){
+		$scope['options'] = $iOptionsData['options'];
+		//$scope.termMeta = <?php echo json_encode( theme_get_term_meta( $vars->term_id ) ) ?>;
+
+		$scope.pwAvatar = <?php echo json_encode($vars) ?>;
+
+		// TODO : Fix issue with digest cycle not updating hidden form field
+
+	}]);
+</script>
+
+<?php //echo json_encode($pwAvatar) ?>
+
+<h3><i class="icon-postworld"></i> Avatar</h3>
+
+<div ng-app="postworldAdmin">
+	
+	<table
+		class="form-table"
+		ng-controller="pwUsermetaAvatarCtrl">
+		<tr>
+			<th><label for="twitter">Select an image</label></th>
+			<td>
+				<?php
+					echo pw_select_image_id( array( 
+						'ng_model'		=>	'pwAvatar',
+						'slug'			=>	'pwAvatar',
+						'label'			=>	'Avatar Image',
+						'width'			=>	'256px',
+					 	));?>
+
+				<input
+					type="hidden"
+					name="pw_avatar"
+					id="pw_avatar"
+					value="{{ pwAvatar }}">
+			</td>
+		</tr>
+
+	</table>
+
+</div>
