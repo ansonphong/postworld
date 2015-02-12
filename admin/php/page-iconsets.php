23b107131e07628192af3251a4072d3cbe38572b
phong <media@phong.com>
2015-02-12 18:33:58 -0500

    dev

--- admin/php/page-iconsets.php
+++ admin/php/page-iconsets.php
@@ -1,7 +1,8 @@
 <div
 	pw-admin
 	pw-admin-iconsets
-	ng-cloak>
+	ng-cloak
+	class="postworld">
 	<h1>
 		<i class="icon-circle-medium"></i>
 		Iconsets
@@ -10,8 +11,12 @@
 	<li>ADD OPTION : Enable Iconset Shortcodes (select from iconsets)</li>
 	<li>ADD OPTION : Clear iconset cache (delete all options)</li>
 
-	<hr>
-
-	<pre><code><?php echo json_encode( pw_get_iconsets(), JSON_PRETTY_PRINT ) ?></code></pre>
+	<?php if( pw_dev_mode() ): ?>
+		<hr>
+		<div class="well">
+			<h3>Loaded Iconsets</h3>
+			<pre><code><?php echo json_encode( pw_get_iconsets(), JSON_PRETTY_PRINT ) ?></code></pre>
+		</div>
+	<?php endif; ?>
 
 </div>
