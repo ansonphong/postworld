18bd3862ff2246fcb8e807901a183dab6935996a
phong <media@phong.com>
2015-04-08 15:13:02 -0400

    dev

--- js/directives/pwUsers.js
+++ js/directives/pwUsers.js
@@ -6,7 +6,6 @@
   \___/|___/\___|_|  |___/
                           
 */
-
 ///// USERS /////
 postworld.directive( 'pwUsers', [ '_', 'pwData', '$log', function( $_, $pwData, $log ){
 	return {
@@ -15,21 +14,44 @@ postworld.directive( 'pwUsers', [ '_', 'pwData', '$log', function( $_, $pwData, 
 			/*
 			attrs.$observe( '', function( userId ){
 			});
+			attrs.$observe( 'pwUserId', function( userId ){
+				if( !_.isEmpty( userId ) ){
+					$scope.user = $pwData.users[ userId ];
+				}
+			});
 			*/
 		},
 	};
 }]);
 
-
 ///// USER /////
-postworld.directive( 'pwUser', [ '_', 'pwData', '$log', function( $_, $pwData, $log ){
+// Directive procudes methods to access the currently logged in user
+
+postworld.directive( 'pwUser',
+	[ '_', 'pwData', '$log', '$pw',
+	function( $_, $pwData, $log, $pw ){
+
 	return {
+		scope:{
+			pwUser:"=",
+			//userFunctions:"="
+		},
 		link: function( $scope, element, attrs  ){
-			attrs.$observe( 'pwUser', function( userId ){
-				if( !_.isEmpty( userId ) ){
-					$scope.user = $pwData.users[ userId ];
-				}
-			});
+
+			// Set the user data
+			var user = $_.get($pw,'user');
+			if( !_.isEmpty(user) )
+				$scope.pwUser = $pw.user;
+			else
+				$scope.pwUser = {};
+
+			// Returns boolean if the user is logged in
+			$scope.pwUser.isLoggedIn = function(){
+				var userId = $_.get( $pw, 'user.data.ID' );
+				return ( userId !== false ) ? true : false;
+			}
+
 		},
 	};
+
 }]);
