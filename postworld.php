c5980de1d0e4d6368afafa5f7604f297c9f92b24
phong <media@phong.com>
2015-04-16 19:13:11 -0400

    version up

--- postworld.php
+++ postworld.php
@@ -23,7 +23,7 @@ function pw_config(){
 global $pw;
 $pw = array(
 	'info'	=>	array(
-		'version'		=>	1.91,
+		'version'		=>	1.92,
 		'db_version'	=>	1.196,
 		'mode'	=>	pw_mode(),
 		'slug'	=>	'postworld',
