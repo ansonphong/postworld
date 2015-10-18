ec7a09fb19c44665df8d1991c64e2d7726cb55b4
phong <media@phong.com>
2015-10-18 01:09:28 -0700

    dev pwColors

--- js/components/pwColors.js
+++ js/components/pwColors.js
@@ -25,40 +25,61 @@ postworld.factory( '$pwColors', [ '$pw', '_', function( $pw, $_ ){
 		},
 
 		/**
-		 * Selects a particular percentage point from a profile of colors.
-		 */
-		getColorPercentage: function( percentage, colorProfile ){
-
-
-
-		},
-
-		/**
-		 * Parse color functions embedded in property values.
-		 * hex(profile.50), rgb(profile.50), rgba(profile.50,.8) 
-		 */
-		parseColorFunctions: function( propertyInner ){
-			
-			console.log( 'parseColorFunctions(propertyInner)', propertyInner );
-
-			function hex(){
-				return 777;
-			}
-			function rgba(){
-				return 888;
-			}
-
-			return eval(propertyInner);
-
-		},
-
-		/**
 		 * Parse a single property value, and replace instances
 		 * Of content appearing between {{ }}
 		 */
 		parsePropertyValue: function( propertyValue, colorProfiles ){
 
-			//var parseColorFunctions = this.parseColorFunctions;
+			/**
+			 * Get a percentage through an array of colors.
+			 * Percentage is parsed as 0%, the first array index,
+			 * 100% the last array index and everything in between.
+			 *
+			 * @param [integer] percentage (0-100) Percent through the array to return. 
+			 * @param [array] colors An array of processed color objects.
+			 */
+			var getColorPercentageFromProfile = function( percentage, colors ){
+				var colorCount = colors.length;
+				var decimal = percentage / 100;
+				var index = Math.floor( decimal * (colorCount-1) );
+				return colors[index];
+			}
+
+			/**
+			 * Gets a color from a profile where the format is provided
+			 * Period deliniated, where the first value is the profile key
+			 * And the second value is the percentage through that profile.
+			 *
+			 * @param string profilePercentage Example. "dynamic.100"
+			 * @return object Processed color object, with hex, rgb, and hsl keys.
+			 */
+			var getColorFromProfile = function( profilePercentage ){
+				var matches = profilePercentage.split('.');
+				var key = matches[0];
+				var colorProfile = colorProfiles[key]
+				return getColorPercentageFromProfile( parseInt(matches[1]), colorProfile.colors );
+			}
+
+			/**
+			 * Parse various color functions to return color values.
+			 * These child functions are used in the special synatax
+			 * used by dynamic style definitions.
+			 */
+			var parseColorFunctions = function( content ){
+
+				function hex( profilePercentage ){
+					var c = getColorFromProfile( profilePercentage );
+					return c['hex'];
+				}
+
+				function rgba( profilePercentage, alpha ){
+					var c = getColorFromProfile( profilePercentage );
+					return 'rgba('+c.rgb[0]+','+c.rgb[1]+','+c.rgb[2]+','+alpha+')';
+				}
+
+				return eval( content );
+
+			}
 
 			// Process the contents of each double curly braces
 			var newPropertyValue = propertyValue.replace( /\{\{(.*?)\}\}/g, function(content){
@@ -75,31 +96,15 @@ postworld.factory( '$pwColors', [ '$pw', '_', function( $pw, $_ ){
 				console.log( 'hasFunction', hasFunction );
 
 				// If it has a function, package and process it with parseColorFunctions()
-
-
+				if( hasFunction ){
+					content = parseColorFunctions( content );
+				}
 				// Otherwise, examine the contents of the content, and do otherwise (?)
-
 
 				return content;
 			});
 
-			/*
-			console.log( 'REGEX', x );
-
-			function hex( val ){
-				return '#fff';
-			}
-
-			function rgba(){
-				return 'rgba(255,255,255,1)';
-			}
-			
-			var evalOutput = eval( x );
-
-			console.log( 'EVAL', evalOutput );
-			*/
-
-			return newPropertyValue;// '#fff';
+			return newPropertyValue;
 
 		},
 
@@ -146,7 +151,7 @@ postworld.directive('pwColors', [ '$pw', '_', '$pwColors', function( $pw, $_, $p
 					return false;
 				return $pwColors.getColorByTag( post, profile, tag ).hex;
 			}
-			
+
 			$scope.getRGBA = function( post, profile, tag, alpha ){
 				if( _.isEmpty( post ) || _.isNull( profile ) )
 					return false;
