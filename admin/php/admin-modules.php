c30444eb8a189c320c8d8677ce89637a02c10f3b
phong <media@phong.com>
2015-11-13 20:44:47 -0800

    bugfix

--- admin/php/admin-modules.php
+++ admin/php/admin-modules.php
@@ -65,14 +65,14 @@ function i_select_menus( $vars ){
 	// DEPRECIATED
 	return pw_select_menus( $vars );
 }
+
 function pw_select_menus( $vars ){
-	/*
-	$vars = array(
-		'options_model' => 	[string] // Angular expression, where to store the results
-		'ng_model' => 		[string] // Angular expression, where to set the option
-		'null_option' =>	[string] // What to label the null option
+	$default_vars = array(
+		'options_model' => 	'options.menus',	//[string] // Angular expression, where to store the results
+		'ng_model' 		=> 	'',					//[string] // Angular expression, where to set the option
+		'null_option' 	=>	'No Menu',			//[string] // What to label the null option
 		);
-	*/
+	$vars = array_replace($default_vars, $vars);
 	return pw_ob_admin_template( 'select-menu', $vars );
 }
 
