55363062797fad3237a4173f7ef8f6d5c6b87fd9
phong <media@phong.com>
2015-12-30 22:03:31 -0800

    version up, added support for custom defaults

--- postworld.php
+++ postworld.php
@@ -25,7 +25,7 @@ global $pwSiteGlobals;
 $pw = array(
 	'config' => $pwSiteGlobals,
 	'info'	=>	array(
-		'version'		=>	1.32,
+		'version'		=>	1.34,
 		'db_version'	=>	1.28,
 		'mode'	=>	pw_mode(),
 		'slug'	=>	'postworld',
