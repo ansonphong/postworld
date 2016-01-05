ea215ba48f14107c720ac0fbdf86c4b483658b39
phong <media@phong.com>
2016-01-04 23:11:46 -0800

    dev

--- postworld.php
+++ postworld.php
@@ -25,7 +25,7 @@ global $pwSiteGlobals;
 $pw = array(
 	'config' => $pwSiteGlobals,
 	'info'	=>	array(
-		'version'		=>	1.35,
+		'version'		=>	1.36,
 		'db_version'	=>	1.28,
 		'mode'	=>	pw_mode(),
 		'slug'	=>	'postworld',
