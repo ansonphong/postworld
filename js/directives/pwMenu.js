750a9cf9a1a72aef41ed3536d78dc60c809ba78f
phong <media@phong.com>
2016-03-17 21:31:31 -0700

    clean menu

--- js/directives/pwMenu.js
+++ js/directives/pwMenu.js
@@ -7,48 +7,29 @@
  ///////////////////////// MENU LOADING DIRECTIVE ////////////////////////*/
 
 // Gets the site menus and populates into the local scope
-postworld.directive( 'pwMenus', function($scope){
+postworld.directive( 'pwMenus', function( $window, $timeout, $pwData, $log, $_, $pw ){
 	return {
 		restrict: 'AE',
-		controller: 'pwMenusCtrl',
 		scope:{
-			pwMenus:'=pwMenus',	// Where to populated the result
+			pwMenus:'=',	// Where to populated the result
 		},
 		link: function( $scope, element, attrs ){
-			// OBSERVE Attribute
-			//attrs.$observe('imageId', function(value) {
-			//	$scope.getImage($scope.imageId);
-			//});
+			$scope.getMenus = function(){
+				// If the menus are preloaded
+				var menus = $_.get( $pw, 'admin.menus' );
+				if( menus != false ){
+					$scope.pwMenus = menus;
+					return;
+				}
+				// If the data isn't preloaded, get it by AJAX
+				$pwData.get_menus({}).then(
+					function(response) {    
+						$scope.pwMenus = response.data;
+					},
+					function(response) {}
+				);
+			};
+			$scope.getMenus();
 		}
 	};
-});
-
-postworld.controller( 'pwMenusCtrl',
-	function( $scope, $window, $timeout, $pwData, $log, $_, $pw ) {
-
-	$scope.getMenus = function(){
-
-		// If the menus are preloaded
-		var menus = $_.get( $pw, 'admin.menus' );
-		if( menus != false ){
-			$scope.pwMenus = menus;
-			return;
-		}
-
-		// If the data isn't preloaded, get it by AJAX
-		$pwData.get_menus({}).then(
-			// Success
-			function(response) {    
-				//$log.debug("MENUS RESPONSE : ", response.data);
-				$scope.pwMenus = response.data;
-			},
-			// Failure
-			function(response) {
-				//$scope.movements = [{post_title:"Movements not loaded.", ID:"0"}];
-			}
-		);
-
-	};
-	$scope.getMenus();
-
 });
